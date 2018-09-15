4e7e3164419920e0ecdfcb65be7debb4f3057899
Slime_hatena <Slime-hatena@users.noreply.github.com>
2018-09-15 18:14:51 +0900

    テンプレートを追加

---
+++ _Template/_template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
---
+++ _Template/_template.php
@@ -0,0 +1,41 @@
+<?php
+class In {
+    private $array = [];
+    private $length = 0;
+    private $count = 0;
+
+    function __construct()	{
+		$str = trim(fgets(STDIN));
+        $this->array = explode(' ', $str);
+        $this->length = count($this->array);
+    }
+    
+    public function get(int $i){
+        $this->array[$i];
+    }
+
+    public function next() {
+        return $this->array[$this->count++];
+    }
+    public function hasNext() {
+        return $this->count < $this->length;
+    }
+    public function nextInt() {
+        return (int)$this->next();
+    }
+    public function nextDouble() {
+        return (double)$this->next();
+    }
+    public function nextString() {
+        return (string)$this->next();
+    }
+}
+
+class Out {
+    public static function line($str = "") {
+        echo $str . PHP_EOL;
+    }
+}
+
+$in = new In();
+Out::line($in->nextInt());
