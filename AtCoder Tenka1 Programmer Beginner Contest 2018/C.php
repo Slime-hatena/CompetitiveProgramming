a4fbe6b01cf2176b2735557bb3065d657038a67c
Slime_hatena <Slime-hatena@users.noreply.github.com>
2018-10-28 05:44:23 +0900

    update AtCoder Tenka1 Programmer Beginner Contest 2018 C

--- AtCoder Tenka1 Programmer Beginner Contest 2018/C.php
+++ AtCoder Tenka1 Programmer Beginner Contest 2018/C.php
@@ -1,97 +1,49 @@
 <?php
-class In {
-    public $array = [];
-    function __construct()	{
-        while (!feof(STDIN)) {
-            $str = trim(fgets(STDIN));
-            $this->array = array_merge($this->array, explode(' ', $str));
-        }
-        $this->length = count($this->array);
-    }
-}
- 
-class Out {
-    public static function line($str = "") {
-        echo $str . PHP_EOL;
-    }
-}
- 
- 
-$in = new In();
-array_splice($in->array, 0, 1);
- 
-$length = count($in->array);
-sort($in->array);
-$large = $in->array;
-$small = array_splice($large, 0, $length / 2);
-rsort($large);
- 
-$flag = (bool)($length % 2);
- 
-$ans = "";
-$smallCnt = 0;
-$largeCnt = 0;
-$smallLength = count($small);
-$largeLength = count($large);
- 
-$flag = !$flag;
- 
-if($flag){
-    $ans = $large[$largeCnt++];
-}else{
-    $ans = $small[$smallCnt++];
-}
-$flag = !$flag;
- 
-$switch = 0;
-while (true) {
-    $left = substr($ans, 0, 1);
-    $right = substr($ans, -1, 1);
- 
-    if($flag){
-        if($largeCnt >= $largeLength){
-            $switch = 0;
-            $flag != $flag;
-        }else{
-            $t = $large[$largeCnt++];
-            if(abs($t - $left) > abs($t - $right)){
-                $ans = $t . "," . $ans;
-            }else{
-                $ans = $ans . "," . $t;
-            }
-            ++$switch;
-        }
- 
+$array = [];
+$first = false;
+while (!feof(STDIN)) {
+    $str = trim(fgets(STDIN));
+
+    if(!$first){
+        $length = (int)$str;
+        $first = true;
     }else{
-        if($smallCnt >= $smallLength){
-            $switch = 0;
-            $flag = !$flag;
-        }else{
-            $t = $small[$smallCnt++];
-            if(abs($t - $left) > abs($t - $right)){
-                $ans = $t . "," . $ans;
-            }else{
-                $ans = $ans . "," . $t;
-            }
-            ++$switch;
-        }
-    }
- 
-    if($switch >= 2){
-        $switch = 0;
-        $flag = !$flag;
-    }
- 
- 
-    if($smallCnt >= $smallLength && $largeCnt >= $largeLength){
-        break;
+        $array[] = (int)$str;
     }
 }
-$arr = explode(',', $ans);
- 
-$ans = 0;
-for ($i = 0; $i < $length - 1; ++$i) { 
-    $ans += ($arr[$i] > $arr[$i + 1]) ? $arr[$i] - $arr[$i + 1] : $arr[$i + 1] - $arr[$i];
+
+sort($array);
+
+$ans0[] = -1;
+$ans1[] = 1;
+
+for ($i = 1; $i < $length - 1; ++$i) { 
+    if((bool)($i % 2)){
+        $ans0[] = 2;
+        $ans1[] = -2;
+    }else{
+        $ans0[] = -2;
+        $ans1[] = 2;
+    }
 }
- 
-Out::line($ans);
+
+if((bool)($length % 2)){
+    $ans0[] = -1;
+    $ans1[] = 1;
+}else{
+    $ans0[] = 1;
+    $ans1[] = -1;
+}
+
+sort($ans0);
+sort($ans1);
+
+$a0 = 0;
+$a1 = 0;
+
+for ($i = 0; $i < $length; ++$i) { 
+    $a0 += $array[$i] * $ans0[$i];
+    $a1 += $array[$i] * $ans1[$i];
+}
+
+echo max($a0, $a1) . PHP_EOL;
